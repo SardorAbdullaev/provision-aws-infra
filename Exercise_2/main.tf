a6cfccb021d83526dd97afc0fca681a403d8f3d4
Sardor (private) <sardorabdullaev91@gmail.com>
2021-04-11 22:07:00 +0200

    added Task4 Task5

---
+++ Exercise_2/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  token = ""
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "greet_lambda" {
+  filename      = "greet_lambda.zip"
+  function_name = "greet_lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
+  runtime       = "python3.6"
+  handler          = "greet_lambda.lambda_handler"
+  source_code_hash = filebase64sha256("greet_lambda.zip")
+  environment {
+    variables = {
+      greeting = "Hello "
+    }
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.example,
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/greet_lambda"
+  retention_in_days = 14
+}
